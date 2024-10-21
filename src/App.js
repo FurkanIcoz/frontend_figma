52b10b6cf8cfc3927b2c0c29b602e2d2397ad0fe
FURKAN İÇÖZ <furkanicoz@outlook.com>
2024-10-22 00:20:50 +0300

    figma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from "react";
 import { Button, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import "./App.css";
+
 function App() {
   return (
     <Container fluid className="bg-linear">
       <Navbar bg="transparent" expand="lg" className="header-nav">
-        <Container>
-          <Navbar.Brand href="#">Collers</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto menu-items">
-              <Nav.Link href="#">Products</Nav.Link>
-              <Nav.Link href="#">Solutions</Nav.Link>
-              <Nav.Link href="#">Pricing</Nav.Link>
-              <Nav.Link href="#">Resources</Nav.Link>
-              <Nav.Link href="#">Log In</Nav.Link>
-              <Nav.Link className="sign-up-btn">Sign up now</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Navbar.Brand href="#">Collers</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto menu-items">
+            <Nav.Link href="#" className="nav-link">Products</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Solutions</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Pricing</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Resources</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Log In</Nav.Link>
+            <Nav.Link href="#" className="sign-up-btn">Sign up now</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
+
+      <Container className="heroBody">
+        <Col className="herocol1 secText">
+          <Row className="ColSneakers">
+            <p className="ColSneakersText">Collectible Sneakers</p>
+            
+          </Row>
+          <Row className="ColSneakers">
+            <p className="ColSneakersParag">Sit elit feugiat turpis sed integer integer accumsan turpis. Sed suspendisse nec lorem mauris. Pharetra, eu imperdiet ipsum ultrices amet.</p>
+            
+          </Row>
+          <Row className="heroButtonsGroup">
+          </Row>
+        </Col>
+        <Col>
+
+        </Col>
+      </Container>
     </Container>
   );
 }
